1caf9483b91ec8a70f97fe60ddf599080718e41a
armandoltx <armando@armandoltx.com>
2019-04-14 12:58:01 +1000

    creado componente Formulario y pasado datos desde el componente Formulario al componente App

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,53 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import Formulario from './Formulario';
 import '../css/App.css';
 
 class App extends Component {
+
+  // crear un state inicial con constructor o sin el:
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     presupuesto: '',
+  //     gastos: {},
+  //     restante: '',
+  //   }
+  // }
+    state = {
+      presupuesto: '',
+      gastos: {},
+      restante: '',
+    }
+
+
+  // Agregar el gasto al state
+  agregarGasto = (gasto) =>{
+    console.log("dentro de app ", gasto);
+
+    //Hacer una copia del state actual
+    const gastos = {...this.state.gastos};
+
+    // Agregar el gasto al objeto del state
+    gastos[`gasto${Date.now()}`] = gasto;
+    //console.log(gastos);
+
+    // ponerlo en el state
+
+    this.setState({
+      gastos: gastos,
+    });
+   }
+
   render() {
     return (
       <div className="App container">
         <Header titulo="Gasto Semanal" />
         <div className="contenido-principal contenido">
           <div className="row">
-            <div className="one-half column"></div>
+            <div className="one-half column">
+              <Formulario agregarGasto={this.agregarGasto} />
+            </div>
             <div className="one-half column"></div>
           </div>
         </div>
---
+++ src/components/Formulario.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+
+class Formulario extends Component {
+
+  // refs para los campos del formulario
+
+  nombreGasto = React.createRef();
+  cantidadGasto = React.createRef();
+
+
+  crearGasto = (e) => {
+    // 1. Prevenir default
+    e.preventDefault();
+    console.log("Dentro de crearGasto");
+
+    // 2. Crear el Objeto con los datos
+    //console.log("nombreGasto ", this.nombreGasto.current.value);
+     const gasto = {
+       nombreGasto: this.nombreGasto.current.value,
+       cantidadGasto: this.cantidadGasto.current.value,
+     }
+     // console.log("gasto ", gasto);
+
+    // 3. Agregarlo y enviarlo por props
+    this.props.agregarGasto(gasto);
+
+    // 4. Resetear el formulario
+    e.currentTarget.reset();
+  }
+
+  render () {
+    return (
+      <form onSubmit={this.crearGasto}>
+        <h2>Agrega tus datos aqui</h2>
+        <div className="campo">
+          <label>Nombre Gasto</label>
+          <input className="u-full-width" type="text" placeholder="Ej. Transporte" ref={this.nombreGasto} />
+        </div>
+
+        <div className="campo">
+          <label>Cantidad</label>
+          <input className="u-full-width" type="text" placeholder="Ej. 300" ref={this.cantidadGasto} />
+        </div>
+
+        <input type="submit" className="button-primary u-full-width" value="Agregar" />
+      </form>
+    );
+  }
+}
+
+export default Formulario;
+
+
