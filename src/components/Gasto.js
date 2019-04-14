3425a33175d2b66af0d1959578fa84b424fa74aa
armandoltx <armando@armandoltx.com>
2019-04-14 18:01:24 +1000

    incorporados los props usando destructuring

--- src/components/Gasto.js
+++ src/components/Gasto.js
@@ -3,9 +3,16 @@ import React, {Component} from 'react';
 
 class Gasto extends Component {
   render () {
+    // console.log("Desde Gasto ", this.props.gasto);
+    const {nombreGasto, cantidadGasto} = this.props.gasto;
     return (
-      <p>desde Gasto</p>
-    );
+      <li className="gastos">
+        <p>
+          {nombreGasto}
+          <span className="gasto">$ {cantidadGasto}</span>
+        </p>
+      </li>
+      );
   }
 }
 
