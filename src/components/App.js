a1d6ff95ed267aa6fbbaea350a1b930f09bf9166
armandoltx <armando@armandoltx.com>
2019-04-14 12:00:15 +1000

    Creado Header y los contenedores en App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import Header from './Header';
 import '../css/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        hola
+      <div className="App container">
+        <Header titulo="Gasto Semanal" />
+        <div className="contenido-principal contenido">
+          <div className="row">
+            <div className="one-half column"></div>
+            <div className="one-half column"></div>
+          </div>
+        </div>
+      
       </div>
     );
   }
