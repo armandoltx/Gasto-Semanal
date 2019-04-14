e101f3e30179ca4305b0dd8fe8d8da0ad8b8021e
armandoltx <armando@armandoltx.com>
2019-04-14 17:53:41 +1000

    Agregando el componente gasto y pasandole los props

---
+++ src/components/Gasto.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+
+class Gasto extends Component {
+  render () {
+    return (
+      <p>desde Gasto</p>
+    );
+  }
+}
+
+
+export default Gasto;
--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Gasto from './Gasto';
 
 class Listado extends Component {
 
@@ -7,6 +8,14 @@ class Listado extends Component {
       <div className="gastos-realizados">
         <h2>Listado</h2>
         {console.log("Desde Listado ", this.props.gastos)}
+        {Object.keys(this.props.gastos).map((key) => (
+          //{/* console.log("key => ", key) */}
+          //{/* console.log("desde dentro del objeto ", this.props.gastos[key]) */}
+          <Gasto
+            key={key}
+            gasto={this.props.gastos[key]}
+          />
+        ))}
       </div>
     );
   }
